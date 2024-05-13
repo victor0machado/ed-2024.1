c00ba261eed753358302acd7958b9089a7880c4f
VICTOR MACHADO DA SILVA <00339239140@professores.ibmec.edu.br>
2024-05-13 11:29:32 -0300

    Exercícios

--- aulas/16-arvores/arvores.go
+++ aulas/16-arvores/arvores.go
@@ -40,8 +40,72 @@ func (n *No) altura() int {
 	return alt2 + 1
 }
 
+func (n *No) busca(valor int) *No {
+	if n == nil { return nil }
+	if n.valor == valor { return n }
+
+	noProcurado := n.esq.busca(valor)
+	if noProcurado != nil { return noProcurado }
+
+	return n.dir.busca(valor)
+}
+
+func (n *No) buscaPai(noPai *No, valor int) (*No, *No) {
+	if n == nil { return nil, noPai }
+	if n.valor == valor { return n, noPai }
+
+	noProcurado, noPai := n.esq.buscaPai(n, valor)
+	if noProcurado != nil { return noProcurado, noPai }
+
+	return n.dir.buscaPai(n, valor)
+}
+
 type Arvore struct {
 	raiz *No
+}
+
+func (a *Arvore) excluir(valor int) error {
+	if a.raiz == nil { return fmt.Errorf("Árvore vazia") }
+
+	noProcurado, noPai := a.raiz.buscaPai(nil, valor)
+	if noProcurado == nil { return fmt.Errorf("Nó não se encontra na árvore") }
+
+	if noPai == nil {
+		a.raiz = nil
+	} else if noPai.esq == noProcurado {
+		noPai.esq = nil
+	} else {
+		noPai.dir = nil
+	}
+
+	return nil
+}
+
+func (a *Arvore) inserir(valorInserir, valorPai int, posicao string) error {
+	if a.raiz == nil { return fmt.Errorf("Árvore vazia") }
+
+	if a.busca(valorInserir) != nil { return fmt.Errorf("Árvore já possui esse valor") }
+
+	noPai := a.busca(valorPai)
+	if noPai == nil { return fmt.Errorf("O nó pai não existe na árvore") }
+
+	switch posicao {
+	case "esq":
+		if noPai.esq != nil { return fmt.Errorf("O nó pai já possui filho à esquerda") }
+		noPai.esq = &No{valor: valorInserir}
+	case "dir":
+		if noPai.dir != nil { return fmt.Errorf("O nó pai já possui filho à direita") }
+		noPai.dir = &No{valor: valorInserir}
+	default:
+		return fmt.Errorf("Posição informada não é válida.")
+	}
+
+	return nil
+}
+
+func (a *Arvore) busca(valor int) *No {
+	if a.raiz == nil { return nil }
+	return a.raiz.busca(valor)
 }
 
 func (a *Arvore) percurso(tipo string) {
@@ -71,11 +135,27 @@ func main() {
 	n2.esq = n4
 	n4.esq = n5
 
-	arv.percurso("pre")
-	fmt.Println("-----------------")
-	arv.percurso("pos")
+	// arv.percurso("pre")
+	// fmt.Println("-----------------")
+	// arv.percurso("pos")
+	// fmt.Println("-----------------")
+	// arv.percurso("sim")
+	// fmt.Println("-----------------")
+	// fmt.Println(arv.altura())
+
+	// fmt.Println(arv.busca(8))
+	// fmt.Println(arv.busca(25))
+
+	fmt.Println(arv.inserir(13, 5, "esq")) // ok
+	fmt.Println(arv.inserir(22, 5, "esq")) // já tem filho à esquerda
+	fmt.Println(arv.inserir(22, 6, "esq")) // nó pai não existe
+	fmt.Println(arv.inserir(13, 5, "dir")) // nó já existe
+	fmt.Println(arv.inserir(22, 5, "abc")) // posição inválida
+	fmt.Println(arv.inserir(22, 10, "dir")) // já tem filho à direita
+
+	arv.percurso("sim")
+
+	arv.excluir(10)
 	fmt.Println("-----------------")
 	arv.percurso("sim")
-	fmt.Println("-----------------")
-	fmt.Println(arv.altura())
 }
